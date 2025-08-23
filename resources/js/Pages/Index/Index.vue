e882d71fec68b5592f9cf803a2f89f2ed13d376e
Hirvaeron <bodnar.alex07@gmail.com>
2025-08-23 17:13:17 +0200

    Kezdeti feltöltés - VILT stack létrehozva

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { ref } from 'vue';
+const counter = ref(0);
+setInterval(() => counter.value++, 1000);
+</script>
+<template>
+    <h1 class="text-3xl">Hello {{ counter }}!</h1>
+</template>
